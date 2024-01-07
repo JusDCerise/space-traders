149b6e4fd76e6cc2e03b46ca6c11f471a1d51d85
Guillaume <guillaume.farbos33@gmail.com>
2024-01-07 16:41:52 +0100

    add: create an account

--- src/routes/Connexion.jsx
+++ src/routes/Connexion.jsx
@@ -5,52 +5,91 @@ export default function Connexion() {
   const [token, setToken] = useState("");
   const [tokenSaisi, setTokenSaisi] = useState("");
   const [symbolJoueur, setSymbolJoueur] = useState("");
+  const [username, setUsername] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     setToken(tokenSaisi);
   }, [tokenSaisi]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const handleLogin = async (inputToken) => {
     try {
-      const reponse = await fetch("https://api.spacetraders.io/v2/my/agent", {
+      const response = await fetch("https://api.spacetraders.io/v2/my/agent", {
         method: "GET",
         headers: {
           Accept: "application/json",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${inputToken}`,
         },
       });
 
-      if (reponse.ok) {
-        const donnees = await reponse.json();
-        setSymbolJoueur(donnees.data.symbol);
-
-        // Stockez les informations dans le localStorage
-        localStorage.setItem("token", token);
-        localStorage.setItem("symbolDuJoueur", donnees.data.symbol);
-
+      if (response.ok) {
+        const data = await response.json();
+        setSymbolJoueur(data.data.symbol);
+        localStorage.setItem("token", inputToken);
+        localStorage.setItem("symbolDuJoueur", data.data.symbol);
         navigate("/");
       } else {
         console.error("Erreur lors de la requête. Veuillez réessayer.");
-        alert("Votre token n'est pas bon, veuilez réessayer");
+        alert("Votre token n'est pas bon, veuillez réessayer");
       }
-    } catch (erreur) {
-      console.error("Erreur lors de la requête :", erreur);
+    } catch (error) {
+      console.error("Erreur lors de la requête :", error);
+    }
+  };
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    handleLogin(tokenSaisi);
+  };
+
+  const handleCreateAccount = async (event) => {
+    event.preventDefault();
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          symbol: username,
+          faction: "COSMIC",
+        }),
+      };
+
+      const response = await fetch("https://api.spacetraders.io/v2/register", options);
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setTokenSaisi(responseData.data.token);
+        handleLogin(responseData.data.token);
+      } else {
+        // console.error("Erreur lors de la requête. Veuillez réessayer.");
+        alert("Ce nom d'utilisateur est déjà pris");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête :", error);
     }
   };
 
   return (
-    <div className="login">
-      <h2>Page de Connexion</h2>
+    <div className="loginPage">
       <div>
-        <form onSubmit={handleSubmit}>
+        <h2>Page de Connexion</h2>
+      </div>
+      <div>
+        <form onSubmit={handleLoginSubmit} className="loginForm">
           <label>
             Token de Connexion :
             <input type="text" value={tokenSaisi} onChange={(e) => setTokenSaisi(e.target.value)} />
           </label>
           <button type="submit">Se connecter</button>
+        </form>
+        <form onSubmit={handleCreateAccount} className="creatAccount">
+          <label>
+            Votre nom d'utilisateur :
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </label>
+          <button type="submit">Créer un compte</button>
         </form>
       </div>
     </div>
